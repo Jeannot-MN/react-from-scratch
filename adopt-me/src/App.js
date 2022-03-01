8a32ec660454cbc69060b65d9d053cd01182392a
JeannotMN <jeannot@ssquared.dev>
2022-03-01 22:12:36 +0200

    babel

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,14 +1,7 @@
 // /* global React ReactDOM */
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.name),
-        React.createElement("h3", {}, props.animal),
-        React.createElement("h3", {}, props.breed),
-    ]);
-};
+import Pet from './Pet';
 
 const App = () => {
     return React.createElement("div", {}, [
