22f2d740242c5b2f9ba285982c4c0350e5def72b
JeannotMN <jeannot@ssquared.dev>
2022-03-01 21:56:39 +0200

    parcel (bundler)

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,4 +1,7 @@
-/* global React ReactDOM */
+// /* global React ReactDOM */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const Pet = (props) => {
     return React.createElement("div", {}, [
         React.createElement("h2", {}, props.name),
